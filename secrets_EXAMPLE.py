7e6b42e34fc2aa5f5be20c1a862fe96f3c204300
Michael Bergeron <mikeb.code@gmail.com>
2020-12-20 00:20:31 +0100

    Add secrets example file.
    Rename secrets_EXAMPLE.py to secrets.py and fill with your info

---
+++ secrets_EXAMPLE.py
@@ -0,0 +1,5 @@
+secrets = {
+    'ssid' :        'SSID_GOES_HERE',           # Keep the two '' quotes around the name
+    'password' :    'PASSWORD GOES HERE',       # Keep the two '' quotes around password
+    'timezone' :    "Europe/Amsterdam",         # http://worldtimeapi.org/timezones
+}
